b4f031b4cf5459a6f7a1fbddf06c933bf46cba0f
taimk-58 <conmuangangqua1919@gmail.com>
2016-09-15 14:23:06 +0700

    update contact plugin

--- wp-content/plugins/uet_contact/uet_contact.php
+++ wp-content/plugins/uet_contact/uet_contact.php
@@ -79,8 +79,8 @@ function uet_contact()
 ?>
 
 <div style="font-weight: bold;font-size:16pt;font-family: 'Roboto', sans-serif;">
-            Quản Lý Thư Từ Thắc Mắc
-</div><br/>
+    Quản Lý Thư Từ Thắc Mắc
+</div><br>
 <div class="table-responsive">
     <?php
          // code phan trang 
@@ -91,32 +91,43 @@ function uet_contact()
         $num_of_pages = ceil( $total / $limit );
         $entries = $wpdb->get_results( "SELECT * FROM wp_contact LIMIT $offset, $limit" );
     ?>
-    <table class="table  table-hover" style="width:99%; ">
-            <tr style="background:#23282d; color:white;font-size:14pt">
-                <th style="width:50px;text-align:center;">STT</th>
-                <th style="text-align:center;">Họ tên</th>
-                <th style="width:50px;text-align:center;">Email</th>
-                <th style="text-align:center;">Nội dung thư</th>
-                <th style="width:150px;text-align:center;">Trạng thái</th>
-                <th style="width:150px;text-align:center;">Chức năng</th>
-                <th style="width:150px;text-align:center;">Trả lời</th>
+    <table class="wp-list-table widefat fixed striped pages"  style="width:99%; font-weight:bold">
+            <tr  style="color:#337ab7;font-size:12pt;border: solid 0.1px #f2f2f2;background-color: #fff">
+                <th style="text-align:center;font-weight: normal;color : #337ab7;width:50px;">STT</th>
+                <th style="text-align:center;font-weight: normal;color : #337ab7;width:150px;">Họ tên</th>
+                <th style="font-weight: normal;color : #337ab7;text-align:center;">Địa chỉ email</th>
+                <th style="text-align:center;font-weight: normal;color : #337ab7">Nội dung thư</th>
+                <th style="text-align:center;font-weight: normal;color : #337ab7;width:150px;">Trạng thái</th>
+                <th style="text-align:center;font-weight: normal;color : #337ab7;width:150px;">Chức năng</th>
+                <th style="text-align:center;font-weight: normal;color : #337ab7;width:150px;">Trả lời</th>
             </tr>
         <?php
+        $count = 0;
         $stt = 1;
+        function my_mb_ucfirst($str) {
+            $fc = mb_strtoupper(mb_substr($str, 0, 1));
+            echo $fc.mb_substr($str, 1);
+        }
         foreach($entries as $value){
+            if($count % 2 != 0){
+                echo '<tr style=" color: #337ab7; background-color:  #fff">';
+            }
+            else{
+                echo '<tr style="; color: #337ab7; background-color: #f2f2f2">';
+            }
             ?>
-            <tr style="font-weight:bold;">
                 <td style="width:50px;text-align:center;"><?= $stt ?></td>
-                <td style="text-align:center;"><?= $value-> name?></td>
-                <td style="text-align:center;"><?= $value-> email?></td>
-                <td style="text-align:center;"><?= $value-> content?></td>
-                <td style="width:150px;text-align:center;"><?= display_status($value-> status) ?></td>
-                <td style="width:150px;text-align:center;"><a class="btn btn-success btn-md" href="<?= "?page=my-unique-identifier&id=$value->id" ?>" >Duyệt thư</a></td>
-             	<td style="width:150px;text-align:center;"><button type="button" class="btn btn-info btn-md" data-toggle="modal" data-target="#myModal" onclick="dialog('<?php echo $value->name?>','<?php echo $value->email?>','<?php echo $value->content?>')" >FeedBack</button></td>
+                <td style="text-align:center;color : #337ab7"><?php $name = $value-> name; my_mb_ucfirst($name);?></td>
+                <td style="text-align:center;color : #337ab7"><?= $value-> email?></td>
+                <td style="text-align:center;color : #337ab7"><?= $value-> content?></td>
+                <td style="width:150px;text-align:center;color : #337ab7"><?= display_status($value-> status) ?></td>
+                <td style="width:150px;text-align:center;"><a class="btn btn-default btn-md" style="color : #337ab7;font-weight:bold;" href="<?= "?page=my-unique-identifier&id=$value->id" ?>" >Duyệt thư</a></td>
+             	<td style="width:150px;text-align:center;"><button type="button" class="btn btn-info btn-md" style="font-weight:bold;" data-toggle="modal" data-target="#myModal" onclick="dialog('<?php echo $value->name?>','<?php echo $value->email?>','<?php echo $value->content?>')" >FeedBack</button></td>
             </tr>
 
             <?php
             $stt++;
+            $count ++;
         }
         ?>
         </table>
@@ -147,26 +158,26 @@ function uet_contact()
 	<!-- Modal -->
 	<div id="myModal" class="modal fade" role="dialog">
 	  <div class="modal-dialog">
-
-	    <!-- Modal content-->
 	    <div class="modal-content">
-	      <div class="modal-header">
-	        <!--<button type="button" class="close" data-dismiss="modal">&times;</button>-->
-	        <!--<h4 class="modal-title">Modal Header</h4>-->
-	        <h3 id= "name" class="modal-title"></h3>
-            <h4 id="email"></h4>
-	      </div>
-	      <div class="modal-body">
-	        <p id ="content" ></p>	
-	      </div>
-          <div class="modal-body">
-            <textarea placeholder="Enter Your FeedBack" rows = 3 style="width:100%"></textarea>
-          </div>
-	      <div class="modal-footer">
-	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-	      </div>
+            <form method="POST" style="font-family:'Roboto', sans-serif;margin-left: 25px;margin-right: 25px;">
+            <div style="font-size:13pt;font-weight:bold;color:#337ab7;text-align:center;margin-top: 10px;">Trả Lời Thư</div><br>
+	        <label style="color:#337ab7;font-weight:normal">Họ tên: 
+                <span style = "font-weight:bold;margin-left: 20px;color:black"id= "name" class="modal-title"></span>
+            </label><br>
+            <label style="color:#337ab7;font-weight:normal">Địa chỉ email:
+                <span style = "font-weight:bold;margin-left: 20px;color:black" id="email"></span>
+            </label><br>
+            
+            <label style="color:#337ab7;font-weight:normal">Nội dung thư:
+                <span style = "font-weight:bold;margin-left: 20px;color:black" id ="content" ></span>
+            </label><br>	
+                <label style="color:#337ab7;font-weight:normal">Nội dung phản hồi</label>
+                <textarea style="font-weight:bold;width:100% ;border-radius:4px;color:black" placeholder="Nhập nội dung" rows = 2 style="width:100%"></textarea>
+	        <div class="modal-footer">
+	           <button style="color : #337ab7;font-weight:bold;" type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	       </div>
+           </form>
 	    </div>
-
 	  </div>
 	</div>
 	
